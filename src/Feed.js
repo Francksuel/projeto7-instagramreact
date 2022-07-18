6fa874f3504b410a1fb63b46266309d9688303a1
Francksuel <francksuelbarbosa@gmail.com>
2022-07-17 23:15:04 -0300

    Requisitos finalizados

--- src/Feed.js
+++ src/Feed.js
@@ -1,25 +1,44 @@
+import React from "react";
+
 const posts=[
     {profileImage:"./Images and videos/the-big-bang-theory.png",user:"bigbangtheory",foto:"./Images and videos/tbbt_meme.jpeg",like:"./Images and videos/rick.png",userLike:"rickandmorty"},
     {profileImage:"./Images and videos/emicida.jpeg",user:"emicida",foto:"./Images and videos/amarelo.jpg",like:"./Images and videos/legiao.jpg",userLike:"legiaourbana"},
     {profileImage:"./Images and videos/Hp.jpg",user:"wizardingworld",foto:"./Images and videos/memeharry.webp",like:"./Images and videos/emicida.jpeg", userLike:"emicida"},
 ]
 
-
 function Post (props){
+    const [heart,setHeart] = React.useState("heart-outline");
+    const [heartColor,setHeartColor] = React.useState("black");
+    
+
+function clicked (object){
+    if (heartColor==="black"){
+        setHeart("heart-sharp");
+        setHeartColor("red");
+        console.log("clicou"); 
+    }else if((heartColor==="red") && (object==="heart")){
+        setHeart("heart-outline");
+        setHeartColor("black");
+        console.log("clicou"); 
+    }
+              
+    }
+    
+    
     return (
         <div class="post">
                     <div class="perfil">
                         <div><img src={props.profileImage}/>
-                            <p> {props.user}</p>
+                            <p> {props.user}</p>                            
                         </div>
                         <div>
                             <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
                         </div>
                     </div>                                      
-                    <div class="foto"><img src={props.foto}/></div>
+                    <div onClick={()=>clicked("post")} class="foto" ><img src={props.foto}/></div>
                     <div class="interacao">
                         <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon onClick={()=>clicked("heart")} style={{color:heartColor}} name={heart}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
@@ -39,7 +58,7 @@ function Post (props){
 function Feed(){
     return (
 <div class="feed">
-{posts.map(posts=><Post profileImage={posts.profileImage} user={posts.user} foto={posts.foto} like={posts.like} userLike={posts.userLike} />)}
+{posts.map(posts=><Post profileImage={posts.profileImage} user={posts.user} foto={posts.foto} like={posts.like} userLike={posts.userLike} class={posts.class} />)}
 </div>
     )
 }
